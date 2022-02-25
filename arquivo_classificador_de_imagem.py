f62acbefc7d8d90eda25839ac4b7edc0636f37f4
OrbeAI <90585189+OrbeAI@users.noreply.github.com>
2022-02-24 22:23:14 -0300

    Update arquivo_classificador_de_imagem.py

--- arquivo_classificador_de_imagem.py
+++ arquivo_classificador_de_imagem.py
@@ -1,43 +1,43 @@
- import tensorflow
- from keras.models import load_model
- from PIL import Image, ImageOps
- import numpy as np
+import tensorflow
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
 
- def funcao_classificar_imagem(img, keras_model):
+def funcao_classificar_imagem(img, keras_model):
 
-   # Disable scientific notation for clarity
-   np.set_printoptions(suppress=True)
+  # Disable scientific notation for clarity
+  np.set_printoptions(suppress=True)
 
-   # Load the model
-   model = load_model('keras_model.h5')
+  # Load the model
+  model = load_model('keras_model.h5')
 
-   # Create the array of the right shape to feed into the keras model
-   # The 'length' or number of images you can put into the array is
-   # determined by the first position in the shape tuple, in this case 1.
-   data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+  # Create the array of the right shape to feed into the keras model
+  # The 'length' or number of images you can put into the array is
+  # determined by the first position in the shape tuple, in this case 1.
+  data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-   # Replace this with the path to your image
-   image = img
+  # Replace this with the path to your image
+  image = img
 
-   #resize the image to a 224x224 with the same strategy as in TM2:
-   #resizing the image to be at least 224x224 and then cropping from the center
-   size = (224, 224)
-   image = ImageOps.fit(image, size, Image.ANTIALIAS)
+  #resize the image to a 224x224 with the same strategy as in TM2:
+  #resizing the image to be at least 224x224 and then cropping from the center
+  size = (224, 224)
+  image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-   #turn the image into a numpy array
-   image_array = np.asarray(image)
+  #turn the image into a numpy array
+  image_array = np.asarray(image)
 
-   # display the resized image
-   image.show()
+  # display the resized image
+  image.show()
 
-   # Normalize the image
-   normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+  # Normalize the image
+  normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-   # Load the image into the array
-   data[0] = normalized_image_array
+  # Load the image into the array
+  data[0] = normalized_image_array
 
-   # run the inference
-   prediction = model.predict(data)
-   print(prediction)
+  # run the inference
+  prediction = model.predict(data)
+  print(prediction)
 
-   return np.argmax(prediction)
+  return np.argmax(prediction)
